dd3d1a6539645d416a3b3d155e203f628033a9ac
Marcelo Daniel Avanzi <90694680+MarceloAvanzi@users.noreply.github.com>
2024-10-11 09:58:30 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import CryptoBlockchain from "./CryptoBlockchain";
 const cryptoBlockChain = new CryptoBlockchain()
 
 console.log('cryptoBlockChain mining in progress...')
-
+ 
 const firstBlock = new CryptoBlock(1, new Date('2024-02-01'), { sender: 'Maria', recipient: 'Abner', quantity: 50 })
 const secondBlock = new CryptoBlock(2, new Date('2024-02-02'), { sender: 'Marcelo Avanzi', recipient: 'Abner2', quantity: 100 })
 
